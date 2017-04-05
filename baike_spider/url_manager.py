e6ddf57a0b5645493aa1f27dc820e621c0c9f1ba
haoziiy <joker.xieyuri@qq.com>
2017-04-05 19:52:01 +0800

    done

--- baike_spider/url_manager.py
+++ baike_spider/url_manager.py
@@ -4,7 +4,7 @@ class UrlManager(object):
         self.old_urls = set()
 
     def add_new_url(self, url):
-        if url == None:
+        if url is None:
             return
         if url not in self.new_urls and url not in self.old_urls:
             self.new_urls.add(url)
